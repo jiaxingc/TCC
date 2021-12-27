852a730dd241721c12a809b1d14519b56782328c
jiaxingc <jiaxing@outlook.com.br>
2021-12-27 16:00:28 -0300

    alterei background no portal

--- addons-lymt/lym_portal/controllers/__init__.py
+++ addons-lymt/lym_portal/controllers/__init__.py
@@ -2,11 +2,11 @@
 
 from . import portal
 from . import myportal
-from . import catalogo
-from . import produto
-from . import video
-from . import curso
-from . import informacao
-from . import banner
-from . import representante
-from . import sala
+# from . import catalogo
+# from . import produto
+# from . import video
+# from . import curso
+# from . import informacao
+# from . import banner
+# from . import representante
+# from . import sala
--- addons-lymt/lym_portal/models/__init__.py
+++ addons-lymt/lym_portal/models/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from . import myportal
-from . import catalogo
-from . import produto
-from . import video
-from . import curso
-from . import informacao
-from . import banner
-from . import representante
-from . import sala
+# from . import catalogo
+# from . import produto
+# from . import video
+# from . import curso
+# from . import informacao
+# from . import banner
+# from . import representante
+# from . import sala
