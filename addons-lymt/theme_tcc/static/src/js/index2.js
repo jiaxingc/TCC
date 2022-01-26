eb6c44d282960bbdf6f7e05466cfb92d75a592e4
Caio Ricciardi <cricciardi@lymtech.com.br>
2022-01-26 10:14:08 -0300

    att

--- addons-lymt/theme_tcc/static/src/js/index2.js
+++ addons-lymt/theme_tcc/static/src/js/index2.js
@@ -48,10 +48,13 @@ $(document).ready(function () {
                         items: 1
                  },
                     768: {
-                        items: 2
+                        items: 4
                     },
                     // 1000: {
                     //     items: 3
+                    // },
+                    // 1200:{
+                    //     items: 4
                     // }
                 }
             });
