02786d9b33a106d10960e55045ed255c86f8c06e
jiaxingc <jiaxing@outlook.com.br>
2022-02-18 17:21:55 -0300

    exibir dados na tela minha inofrmação

--- addons-lymt/agendamento_banco/controllers/portal.py
+++ addons-lymt/agendamento_banco/controllers/portal.py
@@ -25,12 +25,26 @@ class CustomerPortal(http.Controller):
 
     @http.route(['/minhainformacao'], type='http', auth="public", website=True, sitemap=False)
     def minhainformacao(self, **post):
+        id=http.request.env.context.get('uid')
         if request.session.uid:
-        #     minhainformacao = http.request.env['agendamento_banco.minhainformacao'].sudo(
-        #     )
-            return request.render("agendamento_banco.portal_minhainformacao")
+             minhainformacao = http.request.env['res.users'].sudo().search([('id','=',id)])
+             parent_id=minhainformacao.partner_id
+             cliente = http.request.env['res.partner'].sudo().search([('id','=',parent_id.id)])
+        #      banco=http.request.env['res.partner.bank'].sudo().search(('id','=',parent_id.id))
+        #      _logger.info(banco)
+             return request.render("agendamento_banco.portal_minhainformacao",{'cliente':cliente,
+        #      'banco':banco,
+             }
+            )
+                
+                # return request.render("agendamento_banco.portal_minhainformacao")
+            
         else:
             return request.redirect('/web/login')
+
+
+
+
 
 
 portal.CustomerPortal.home = CustomerPortal.home
